96534af9d8f0a393241e2b5d49d9d3e567a35b2d
cuongnguyen1996 <cuongnguyen29696@gmail.com>
2024-10-21 18:56:58 +0700

    Main: entry log error when init app failed

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -23,6 +23,7 @@ export default defineConfig({
     },
   },
   preload: {
+    plugins: [swcPlugin()],
     resolve: resolveD,
     // ...
   },
--- src/main/index.ts
+++ src/main/index.ts
@@ -2,6 +2,9 @@ import { app } from 'electron';
 import { makeAppWithSingleInstanceLock } from './factories';
 import { Application } from './app';
 import { initMenu } from './menu';
+import { createLogger } from './logging';
+
+const logger = createLogger('entry', 'debug');
 
 app.on('certificate-error', (event, webContents, url, error, certificate, callback) => {
   // On certificate error we disable default behaviour (stop loading the page)
@@ -12,7 +15,12 @@ app.on('certificate-error', (event, webContents, url, error, certificate, callba
 app.commandLine.appendSwitch('ignore-certificate-errors');
 
 makeAppWithSingleInstanceLock(async () => {
-  const hicApp = new Application(app, {});
-  initMenu(app);
-  await hicApp.init();
+  try {
+    const hicApp = new Application(app, {});
+    initMenu(app);
+    await hicApp.init();
+  } catch (e) {
+    logger.error('Init application got error', { error: e });
+    app.quit();
+  }
 });
